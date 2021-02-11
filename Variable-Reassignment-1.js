2d0737e9aee06c38538c6fe29e90f0629530df24
JoshuaJHaines <joshjhaines3@gmail.com>
2021-02-11 09:36:17 -0500

    added last nights homework from Repil.it

---
+++ Variable-Reassignment-1.js
@@ -0,0 +1,14 @@
+let x = 3;
+let y = x;
+x = 5;
+
+// What is the value of x after line 1 executes?
+x is 3
+
+// What are the values of x and y after line 2 executes?
+x is 3
+y is 3
+
+// What are the values of x and y after line 3 executes?
+x is 5
+y is 3
